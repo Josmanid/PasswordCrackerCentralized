9c128f4ec89f7482d91812bb36e53669c7d9bc4e
Jonas Martin Smidt <jos005@edu.zealand.dk>
2025-09-29 09:12:42 +0200

    Add project files.

---
+++ PasswordCrackerCentralized/Cracking.cs
@@ -0,0 +1,151 @@
+﻿using PasswordCrackerDecentralizedSlave.model;
+using PasswordCrackerDecentralizedSlave.util;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+namespace PasswordCrackerDecentralizedSlave
+{
+    public class Cracking
+    {
+        /// <summary>
+        /// The algorithm used for encryption.
+        /// Must be exactly the same algorithm that was used to encrypt the passwords in the password file
+        /// </summary>
+        private readonly HashAlgorithm _messageDigest;
+
+        public Cracking()
+        {
+            _messageDigest = new SHA1CryptoServiceProvider();
+            //_messageDigest = new MD5CryptoServiceProvider();
+            // seems to be same speed
+        }
+
+        /// <summary>
+        /// Runs the password cracking algorithm
+        /// Input the lines from chunks and output the result from UserInforClearText klassen
+        /// </summary>
+        public List<UserInfoClearText> RunCracking(string dictionaryLine) {
+            List<UserInfo> userInfos = PasswordFileHandler.ReadPasswordFile("passwords.txt");
+            List<UserInfoClearText> result = new List<UserInfoClearText>();
+
+            // Process just the line sent by the master
+            IEnumerable<UserInfoClearText> partialResult = CheckWordWithVariations(dictionaryLine, userInfos);
+            result.AddRange(partialResult);
+
+       
+
+            return result;
+        }
+
+
+        /// <summary>
+        /// Generates a lot of variations, encrypts each of the and compares it to all entries in the password file
+        /// </summary>
+        /// <param name="dictionaryEntry">A single word from the dictionary</param>
+        /// <param name="userInfos">List of (username, encrypted password) pairs from the password file</param>
+        /// <returns>A list of (username, readable password) pairs. The list might be empty</returns>
+        private IEnumerable<UserInfoClearText> CheckWordWithVariations(string dictionaryEntry, List<UserInfo> userInfos)
+        {
+            List<UserInfoClearText> result = new List<UserInfoClearText>(); //might be empty
+
+            string possiblePassword = dictionaryEntry;
+            IEnumerable<UserInfoClearText> partialResult = CheckSingleWord(userInfos, possiblePassword);
+            result.AddRange(partialResult);
+
+            string possiblePasswordUpperCase = dictionaryEntry.ToUpper();
+            IEnumerable<UserInfoClearText> partialResultUpperCase = CheckSingleWord(userInfos, possiblePasswordUpperCase);
+            result.AddRange(partialResultUpperCase);
+
+            string possiblePasswordCapitalized = StringUtilities.Capitalize(dictionaryEntry);
+            IEnumerable<UserInfoClearText> partialResultCapitalized = CheckSingleWord(userInfos, possiblePasswordCapitalized);
+            result.AddRange(partialResultCapitalized);
+
+            string possiblePasswordReverse = StringUtilities.Reverse(dictionaryEntry);
+            IEnumerable<UserInfoClearText> partialResultReverse = CheckSingleWord(userInfos, possiblePasswordReverse);
+            result.AddRange(partialResultReverse);
+
+            for (int i = 0; i < 100; i++)
+            {
+                string possiblePasswordEndDigit = dictionaryEntry + i;
+                IEnumerable<UserInfoClearText> partialResultEndDigit = CheckSingleWord(userInfos, possiblePasswordEndDigit);
+                result.AddRange(partialResultEndDigit);
+            }
+
+            for (int i = 0; i < 100; i++)
+            {
+                string possiblePasswordStartDigit = i + dictionaryEntry;
+                IEnumerable<UserInfoClearText> partialResultStartDigit = CheckSingleWord(userInfos, possiblePasswordStartDigit);
+                result.AddRange(partialResultStartDigit);
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    string possiblePasswordStartEndDigit = i + dictionaryEntry + j;
+                    IEnumerable<UserInfoClearText> partialResultStartEndDigit = CheckSingleWord(userInfos, possiblePasswordStartEndDigit);
+                    result.AddRange(partialResultStartEndDigit);
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Checks a single word (or rather a variation of a word): Encrypts and compares to all entries in the password file
+        /// </summary>
+        /// <param name="userInfos"></param>
+        /// <param name="possiblePassword">List of (username, encrypted password) pairs from the password file</param>
+        /// <returns>A list of (username, readable password) pairs. The list might be empty</returns>
+        private IEnumerable<UserInfoClearText> CheckSingleWord(IEnumerable<UserInfo> userInfos, string possiblePassword)
+        {
+            char[] charArray = possiblePassword.ToCharArray();
+            byte[] passwordAsBytes = Array.ConvertAll(charArray, PasswordFileHandler.GetConverter());
+
+            byte[] encryptedPassword = _messageDigest.ComputeHash(passwordAsBytes);
+            //string encryptedPasswordBase64 = System.Convert.ToBase64String(encryptedPassword);
+
+            List<UserInfoClearText> results = new List<UserInfoClearText>();
+
+            foreach (UserInfo userInfo in userInfos)
+            {
+                if (CompareBytes(userInfo.EntryptedPassword, encryptedPassword))  //compares byte arrays
+                {
+                    results.Add(new UserInfoClearText(userInfo.Username, possiblePassword));
+                    Console.WriteLine(userInfo.Username + " " + possiblePassword);
+                }
+            }
+            return results;
+        }
+
+        /// <summary>
+        /// Compares to byte arrays. Encrypted words are byte arrays
+        /// </summary>
+        /// <param name="firstArray"></param>
+        /// <param name="secondArray"></param>
+        /// <returns></returns>
+        private static bool CompareBytes(IList<byte> firstArray, IList<byte> secondArray)
+        {
+            //if (secondArray == null)
+            //{
+            //    throw new ArgumentNullException("firstArray");
+            //}
+            //if (secondArray == null)
+            //{
+            //    throw new ArgumentNullException("secondArray");
+            //}
+            if (firstArray.Count != secondArray.Count)
+            {
+                return false;
+            }
+            for (int i = 0; i < firstArray.Count; i++)
+            {
+                if (firstArray[i] != secondArray[i])
+                    return false;
+            }
+            return true;
+        }
+
+    }
+}
---
+++ PasswordCrackerCentralized/Program.cs
@@ -0,0 +1,55 @@
+﻿using PasswordCrackerDecentralizedSlave.model;
+using PasswordCrackerDecentralizedSlave;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace PasswordCrackerDecentralizedSlave
+{
+    class Program
+    {
+        static void Main() {
+            //TCP client slave connection 
+            Console.WriteLine("Slave client is ready to receive the order!");
+            int port = 7;
+            // Connect to the master server on localhost (127.0.0.1)
+            TcpClient client = new TcpClient("127.0.0.1", port);
+
+            // Get the network stream to read and write data
+            NetworkStream stream = client.GetStream();
+            StreamReader streamReader = new StreamReader(stream);
+            StreamWriter streamWriter = new StreamWriter(stream) { AutoFlush = true };
+            // Create an instance of your cracking class
+            Cracking cracker = new Cracking();
+
+
+            // Read each line sent by the master until it signals the end
+            List<UserInfoClearText> allResults = new List<UserInfoClearText>();
+            string line;
+            while ((line = streamReader.ReadLine()) != null)
+            {
+                if (line == "The chunks is empty") break; // stop when the chunk ends
+                // Run cracking and collect results
+                List<UserInfoClearText> results = cracker.RunCracking(line);
+                allResults.AddRange(results);
+
+            }
+
+            // Send results back to the master
+            foreach (var result in allResults)
+            {
+                streamWriter.WriteLine($"{result.UserName}:{result.Password}");
+            }
+
+            // Signal that results are done
+            streamWriter.WriteLine("DONE");
+
+
+            client.Close();
+
+        }
+    }
+}
